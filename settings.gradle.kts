4c1f8d6d81e5cf097e31f52c68ef1b02e4a53495
taism <tmmarambiocapelli@mail.austral.edu.ar>
2025-09-09 17:01:51 -0300

    refactor: Simplify build configuration by removing unnecessary SCM versioning and updating project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "PrintScript2"
+rootProject.name = "PrintScript"
 
 include("linter",
     "cli",
