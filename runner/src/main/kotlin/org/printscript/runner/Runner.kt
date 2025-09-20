945e9f5d73edba23ae8fae6c72609388dc89eabc
taismmc <tmmarambiocapelli@mail.austral.edu.ar>
2025-09-20 01:41:41 -0300

    feat: Update Runner class to implement token processing and execution logic

--- runner/src/main/kotlin/org/printscript/runner/Runner.kt
+++ runner/src/main/kotlin/org/printscript/runner/Runner.kt
@@ -1,14 +1,22 @@
 package org.printscript.runner
 
+import org.printscript.lexer.Lexer
+import org.printscript.lexer.pattern.TokenProvider
+import org.printscript.parser.Parser
+import org.printscript.interpreter.Interpreter
 import java.io.InputStream
+import java.io.InputStreamReader
 
-// TODO: Implementar la clase Runner
-class Runner {
-    fun run(
-        inputStream: InputStream,
-        version: String,
-    ) {
-        val readerIterator = ReaderIterator().getLineIterator(inputStream)
-        // Procesar tokens con el parser y ejecutar lógica
+class Runner(
+    private val tokenProvider: TokenProvider,
+    private val parser: Parser,
+    private val interpreter: Interpreter,
+) {
+    fun run(inputStream: InputStream) {
+        val lexer = Lexer(tokenProvider)
+        val reader = InputStreamReader(inputStream)
+        val tokens = lexer.lex(reader)
+        val ast = parser.parse(tokens)
+        interpreter.execute(ast)
     }
 }
