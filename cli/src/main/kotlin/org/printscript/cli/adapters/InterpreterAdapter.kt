162da22b14e24b49cbf20a9c3a23a63eae9cfd19
Abrojito <abrilbrieva@gmail.com>
2025-12-10 12:42:14 -0300

    cli fixes for use cases

--- cli/src/main/kotlin/org/printscript/cli/adapters/InterpreterAdapter.kt
+++ cli/src/main/kotlin/org/printscript/cli/adapters/InterpreterAdapter.kt
@@ -1,12 +1,21 @@
 package org.printscript.cli.adapters
 
 import org.printscript.interpreter.Interpreter
+import org.printscript.interpreter.io.DefaultOutputProvider
+import org.printscript.interpreter.io.IOContext
+import org.printscript.interpreter.io.OutputProvider
 import org.printscript.parser.node.ASTNode
 
 class InterpreterAdapter(
-    private val interpreter: Interpreter = Interpreter(),
+    private val interpreterFactory: (OutputProvider, IOContext) -> Interpreter = { output, io -> Interpreter(output = output, ioContext = io) },
 ) {
-    fun run(ast: List<ASTNode>) {
+    fun run(
+        ast: List<ASTNode>,
+        ioContext: IOContext = IOContext.systemDefault(),
+        output: OutputProvider = DefaultOutputProvider(),
+    ) {
+        val interpreter = interpreterFactory(output, ioContext)
+        // Ejecutar el AST en un intérprete que usa el IO provisto (stdin/stdout por defecto)
         interpreter.execute(ast)
     }
 }
